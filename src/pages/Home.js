1259917769cf7f66bcacafb5bbc1977159d22d1a
Robby Rice <riceslif@gmail.com>
2022-10-13 14:41:20 -0500

    full draft done!!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,14 +14,14 @@ function Home() {
         <>
             <div className='bg-light-tan background-img bg-center max-h-screen h-full'>
                 <Navbar />
-                <div className='hidden md:flex m-10 max-h-screen flex-col min-h-[85vh]'>
+                <div className='md:flex m-10 max-h-screen flex-col min-h-[85vh]'>
                     <div className='pt-12'>
                         <p className='text-8xl whitespace-pre text-m-green -mx-8 '>robby <br />rice</p>
                     </div>
                     <div className='items-center flex-1 pt-16 max-h-[30vh] align-middle flex justify-end'>
                         <p className='text-5xl lg:text-6xl text-light-tan text-right hover:animate-pulse'>developer && engineer</p>
                     </div>
-                    <div className='flex m-2'>   
+                    <div className='hidden md:flex m-2'>   
                         <div className='h-fit w-2/3 min-h-52 mr-10 flex rounded-2xl border-solid border-2 border-d-green backdrop-blur-xl bg-l-green bg-opacity-20 items-center align-middle'>
                             <p className='text-black text-2xl py-6 px-6'>i'm robby, a student developer. welcome to my portfolio - feel free to snoop around and get in contact.</p>
                         </div>
