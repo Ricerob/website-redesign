fd183d674477e1840057e8d0310e8bd7af624490
Robby <riceslif@gmail.com>
2023-07-05 19:58:50 -0500

    Adding SprintSage

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,5 @@
 import { FaPython, FaJava, FaHtml5, FaReact, FaAws } from "react-icons/fa";
-import {SiJavascript, SiNextdotjs, SiTailwindcss} from 'react-icons/si';
+import {SiJavascript, SiNextdotjs, SiOpenai, SiTailwindcss} from 'react-icons/si';
 
 const projects = [
     {
@@ -8,6 +8,13 @@ const projects = [
         'desc': 'Empowering students with AI automated question generation. PDF parsing. Full-stack web app, AWS backend. Data streams from chat. OpenAI API.',
         'link': 'https://www.swift-ta.com/',
         'linkName': 'Website'
+    },
+    {
+        'name': 'Sprint Sage',
+        'icons': [<FaReact size={60} />, <FaPython size={60}/>, <SiNextdotjs size={50}/>, <SiOpenai size={50} />],
+        'desc': 'Intelligent sprint planning for PMs. Generative AI for feature generation. Collating & vectorizing 6+ data sources. 2023 AI LLM Berkley Hackathon submission.',
+        'link': 'https://devpost.com/software/swift-sage',
+        'linkName': 'Devpost'
     },
     {
         'name': 'track',
